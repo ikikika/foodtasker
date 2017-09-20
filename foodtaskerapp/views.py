c19a2a5c8ac2df0ae6fb6d90aec817dc4a755f1c
ikikika <iki.jito@gmail.com>
2017-09-21 03:36:28 +0800

    order page

--- foodtaskerapp/views.py
+++ foodtaskerapp/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from foodtaskerapp.forms import UserForm, RestaurantForm, UserFormForEdit, MealForm
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
-from foodtaskerapp.models import Meal
+from foodtaskerapp.models import Meal, Order
 # Create your views here.
 
 def home(request):
@@ -65,7 +65,8 @@ def restaurant_edit_meal(request, meal_id):
 
 @login_required(login_url='/restaurant/sign-in/')
 def restaurant_order(request):
-    return render(request, 'restaurant/order.html', {})
+    orders = Order.objects.filter(restaurant = request.user.restaurant).order_by("-id")
+    return render(request, 'restaurant/order.html', {"orders": orders})
 
 @login_required(login_url='/restaurant/sign-in/')
 def restaurant_report(request):
