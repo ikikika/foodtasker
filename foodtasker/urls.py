12c80340dedc5ffd4dfa1e880e28c38026d49f39
ikikika <iki.jito@gmail.com>
2017-09-21 00:39:23 +0800

    create model for meals, makemigrations, migrate

--- foodtasker/urls.py
+++ foodtasker/urls.py
@@ -36,12 +36,14 @@ urlpatterns = [
         name = 'restaurant-sign-up'),
     url(r'^restaurant/$', views.restaurant_home, name = 'restaurant-home'),
 
-
     url(r'^restaurant/account/$', views.restaurant_account, name='restaurant-account'),
+
     url(r'^restaurant/meal/$', views.restaurant_meal, name='restaurant-meal'),
+    url(r'^restaurant/meal/add/$', views.restaurant_add_meal, name='restaurant-add-meal'),
+
     url(r'^restaurant/order/$', views.restaurant_order, name='restaurant-order'),
     url(r'^restaurant/report/$', views.restaurant_report, name='restaurant-report'),
-    
+
 
 
     # sign in/  sign up/ sign out
